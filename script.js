576357a3bc1a8b7624d943d0ee9c86353ff74121
LcEduardo <lucasproject777@gmail.com>
2024-12-21 10:09:24 -0300

    chasing places

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-const btns = document.querySelectorAll("button").values;
+const btns = document.querySelectorAll("button");
+const win = document.getElementById("window");
+const imgElement = document.createElement('img');
 
 btns.forEach(btn => {
+
     btn.addEventListener("click", () => {
-        if (btn.values === "praia") {
-            
+        if (btn.dataset.set === "praia") {
+            imgElement.src = 'imgs/beach.jpg';
+            imgElement.width = 250;
+            imgElement.height = 200;
+            win.appendChild(imgElement);
+        } 
+        else if (btn.dataset.set === "city") {
+            imgElement.src = 'imgs/city.jpg';
+            imgElement.width = 250;
+            imgElement.height = 200;
+            win.appendChild(imgElement);
         }
-    })
+        else if (btn.dataset.set === "snow") {
+            imgElement.src = 'imgs/cold.jpg';
+            imgElement.width = 250;
+            imgElement.height = 200;
+            win.appendChild(imgElement);
+        } 
+        else {
+            imgElement.src = 'imgs/field.jpg';
+            imgElement.width = 250;
+            imgElement.height = 200;
+            win.appendChild(imgElement);
+        }
+    });
 });
