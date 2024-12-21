a48f5c68de925e1768b96d0d33dca0956868ca81
LcEduardo <lucasproject777@gmail.com>
2024-12-21 09:07:22 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const btns = document.querySelectorAll("button").values;
+
+btns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        if (btn.values === "praia") {
+            
+        }
+    })
+});
