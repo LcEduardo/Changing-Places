83c7e0165f391dc3d5fc0e167724b0e9eb413799
LcEduardo <lucasproject777@gmail.com>
2024-12-23 15:49:28 -0300

    Apply window

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
 const btns = document.querySelectorAll("button");
-const win = document.getElementById("window");
+const win = document.getElementById("landscape");
 const imgElement = document.createElement('img');
 
 btns.forEach(btn => {
 
     btn.addEventListener("click", () => {
+        win.style.display = 'block';
+        imgElement.style.borderRadius = 10;
+        imgElement.width = 135;
+        imgElement.height = 128;
         if (btn.dataset.set === "praia") {
             imgElement.src = 'imgs/beach.jpg';
-            imgElement.width = 250;
-            imgElement.height = 200;
             win.appendChild(imgElement);
         } 
         else if (btn.dataset.set === "city") {
             imgElement.src = 'imgs/city.jpg';
-            imgElement.width = 250;
-            imgElement.height = 200;
             win.appendChild(imgElement);
         }
         else if (btn.dataset.set === "snow") {
             imgElement.src = 'imgs/cold.jpg';
-            imgElement.width = 250;
-            imgElement.height = 200;
             win.appendChild(imgElement);
         } 
         else {
             imgElement.src = 'imgs/field.jpg';
-            imgElement.width = 250;
-            imgElement.height = 200;
             win.appendChild(imgElement);
         }
     });
